e1dc49d78a91a8c7f256e584d3ea8b8d0a68e0b0
marceldesktop <marecrisbr@gmail.com>
2018-02-01 23:50:49 -0200

    nao lembro

--- views/contato.php
+++ views/contato.php
@@ -48,7 +48,7 @@ Email: <?php    echo $value = $viewData['email'];  ?></br>
 <hr/>
     <img src="<?php BASE_URL ?>assets/images/foto_estabelecimento_frontal.png" width="200" height="200" class="img-rounded img-responsive">
     
-    </address>
+    
     <div class="mapa">
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3665.963958766659!2d-47.06167538459018!3d-23.244398555849436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf397d341e59af%3A0x9d37d8833d8e721e!2sAv.+S%C3%A3o+Paulo%2C+828+-+Jandim+Andre%2C+Cabre%C3%BAva+-+SP%2C+13315-000!5e0!3m2!1spt-BR!2sbr!4v1503893598450" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
 </div>
@@ -58,6 +58,7 @@ Email: <?php    echo $value = $viewData['email'];  ?></br>
        <div class="mapamobile2">
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3665.963958766659!2d-47.06167538459018!3d-23.244398555849436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf397d341e59af%3A0x9d37d8833d8e721e!2sAv.+S%C3%A3o+Paulo%2C+828+-+Jandim+Andre%2C+Cabre%C3%BAva+-+SP%2C+13315-000!5e0!3m2!1spt-BR!2sbr!4v1503893598450" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
 </div>
+    </address>
     </div>
   
    </div>
